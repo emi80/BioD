0f843dee2b8e685208b3b3595c1fcffbbaec0c2b
Emilio Palumbo <emiliopalumbo@gmail.com>
2019-02-05 11:43:07 +0100

    WIP - add compressed input support

--- bio/std/file/fasta.d
+++ bio/std/file/fasta.d
@@ -34,6 +34,8 @@ import std.typecons;
 import std.exception;
 import std.path;
 import std.file;
+import std.zlib;
+// import bio.std.experimental.hts.bgzf;
 import bio.std.file.fai;
 
 /* 
@@ -56,7 +58,7 @@ import bio.std.file.fai;
   TODO: 
  1. Allow reading gzipped fasta files.
  
-*/
+*/  
 
 struct FastaRecord {
     string header;
@@ -155,15 +157,215 @@ struct Region {
     }
 }
 
+struct FastaIterator {
+    File f;
+    bool gzipped;
+    string lineTerm = "\n";
+    FastaRecord current, next;
+
+    struct Reader {
+        File f;
+        bool gzipped;
+        KeepTerminator kt = KeepTerminator.no;
+        string lineTerm = "\n", line;
+        ubyte[] remind;
+        ubyte[][] buffer;
+        UnCompress decmp = new UnCompress();
+
+        @property bool empty() {
+            return f.eof && buffer.empty;
+        }
+
+        @property ref string front() {
+            if (line.empty) {
+                popFront();              
+            }
+            return line;
+        }
+
+        void popFront() {
+            if ( buffer.length == 0 ) {
+                foreach (chunk; f.byChunk(4096)) {
+                    if ( gzipped )
+                        chunk = cast(ubyte[])decmp.uncompress(chunk);
+                    buffer = chunk.dup.split(lineTerm);
+                    if ( remind ) {
+                        buffer[0]=remind~buffer[0];
+                    }
+                    if ( countUntil(retro(chunk), lineTerm) ) {
+                        remind = buffer[$-1];
+                        buffer = buffer[0..$-1];
+                    }
+                    break;
+                }
+            }
+            foreach (l; buffer) {
+                line = cast(string)l;
+                buffer = buffer[1..$];
+                break;
+            }
+        }
+    }
+
+    unittest {
+        auto testFa = tempDir.buildPath("test.fa");
+        scope(exit) testFa.remove;
+        File(testFa, "w").writeln(q"(
+            >chr1
+            acgtgagtgc
+            >chr2
+            acgtgagtgcacgtgagtgcacgtgagtgc
+            acgtgagtgcacgtgagtgc
+            >chr3 hrsv | Kilifi | partial sequence
+            CATGTTATTACAAGTAGTGATATTTGCCCTAATAATAATATTGTAGTGAAATCCAATTTCACAACAATGC
+        )".outdent().strip());
+        auto r = Reader(File(testFa));
+        r.lineTerm = "\t";
+
+        ubyte[] gzip = [
+            0x1f, 0x8b, 0x08, 0x08, 0xa8, 0x78, 0xf2, 0x5b, 0x00, 0x03, 0x74, 0x31, 0x2e, 0x66, 0x61, 0x00,
+            0x6d, 0x4b, 0x39, 0x0a, 0x80, 0x30, 0x10, 0xec, 0xf3, 0x8a, 0x7c, 0x41, 0xed, 0x85, 0x65, 0x8b,
+            0x2d, 0x6c, 0xf7, 0x03, 0x21, 0x44, 0x0d, 0x88, 0x68, 0x3c, 0x2a, 0x1f, 0xef, 0x44, 0x11, 0x2c,
+            0x02, 0x33, 0x30, 0x67, 0xeb, 0xc7, 0x54, 0x19, 0xe7, 0x87, 0x7d, 0x70, 0xa0, 0x37, 0x2d, 0x82,
+            0xfa, 0x17, 0x94, 0x54, 0xb1, 0x7e, 0x9e, 0x8d, 0x1d, 0xd3, 0x76, 0xda, 0xcb, 0x76, 0x71, 0x8a,
+            0x7d, 0x84, 0x58, 0x5c, 0xda, 0xa3, 0x9b, 0xec, 0x16, 0xd6, 0x23, 0xcc, 0x3e, 0x18, 0x26, 0x15,
+            0x55, 0x02, 0x98, 0x48, 0x14, 0x10, 0xca, 0x56, 0x85, 0x99, 0x95, 0xe8, 0x03, 0x82, 0xb7, 0x84,
+            0x61, 0x4c, 0xb1, 0xe0, 0x7c, 0xc9, 0x52, 0xd8, 0xdc, 0x43, 0x6d, 0x5b, 0xc0, 0xb9, 0x00, 0x00,
+            0x00
+        ];
+        auto testFaGz = tempDir.buildPath("test.fa.gz");
+        scope(exit) testFaGz.remove;
+        File(testFaGz, "w").rawWrite(gzip);
+        // foreach (ubyte[] read; BgzfBlocks(testFaGz)) {
+        //     writeln(cast(string)read); // raw unpacking
+        // }
+        r = Reader(File(testFaGz), true);
+        // foreach(l; r) {
+        //     writeln(l);
+        // }
+    }
+
+    this(string filename) {
+        f = File(filename);
+        detectLineTerm();
+        isGzipped();
+    }
+
+    void isGzipped() {
+        auto pos = f.tell;
+        f.rewind();
+        auto magic = f.rawRead(new ubyte[2]);
+        gzipped = ( magic[0] == to!ubyte(0x1f) && magic[1] == to!ubyte(0x8b) );
+        f.seek(pos);
+    }
+
+    void detectLineTerm() {
+        lineTerm = readLines(KeepTerminator.yes)
+                           .take(1)
+                           .front
+                           .endsWith("\r\n") ? "\r\n" : "\n";
+        f.rewind();
+    }
+
+    auto readLines(KeepTerminator kt = KeepTerminator.no) {
+        return f.byLine(kt, lineTerm);
+    }
+
+    @property bool empty() {
+        return f.eof && current.len == 0;
+    }
+
+    @property ref FastaRecord front() {
+        if ( current.len == 0 ) {
+            popFront();
+        }
+        return current;
+    }
+
+    void popFront() {
+        current = next;
+        foreach(line; readLines()) {
+            if ( line.startsWith(">") ) {
+                if ( current.len > 0 ) {
+                    next = FastaRecord();
+                    next.lineTerm = lineTerm;
+                    next.header ~= line[1..$];
+                    break;
+                }
+                current.lineTerm = lineTerm;
+                current.header ~= line[1..$];
+            } else {
+                if ( current.lineLen == 0 ) {
+                    current.lineLen = line.length;
+                }
+                current.sequence ~= line;
+            }
+        }
+    }
+}
+
+unittest {
+    auto testFa = tempDir.buildPath("test.fa");
+    scope(exit) testFa.remove;
+    File(testFa, "w").writeln(q"(
+        >chr1
+        acgtgagtgc
+        >chr2
+        acgtgagtgcacgtgagtgcacgtgagtgc
+        acgtgagtgcacgtgagtgc
+        >chr3 hrsv | Kilifi | partial sequence
+        CATGTTATTACAAGTAGTGATATTTGCCCTAATAATAATATTGTAGTGAAATCCAATTTCACAACAATGC
+    )".outdent().strip());
+    assert ( FastaIterator(testFa).gzipped == false );
+    auto records = FastaIterator(testFa).array;
+    assert ( records.length == 3 );
+    assert ( records[0].header == "chr1" );
+    assert ( records[0].sequence == "acgtgagtgc" );
+    assert ( records[0].lineLen == 10 );
+    assert ( records[0].toString == q"(
+        >chr1
+        acgtgagtgc
+    )".outdent().strip());
+    assert ( records[1].header == "chr2" );
+    assert ( records[1].sequence == "acgtgagtgcacgtgagtgcacgtgagtgcacgtgagtgcacgtgagtgc" );
+    assert ( records[1].lineLen == 30 );
+    assert ( records[1].toString == q"(
+        >chr2
+        acgtgagtgcacgtgagtgcacgtgagtgc
+        acgtgagtgcacgtgagtgc
+    )".outdent().strip());
+
+    assert(records[2].header == "chr3 hrsv | Kilifi | partial sequence");
+    assert(records[2].headerFields.length == 3);
+    assert(records[2].headerFields == ["chr3 hrsv","Kilifi","partial sequence"]);
+
+    ubyte[] gzip = [
+        0x1f, 0x8b, 0x08, 0x08, 0xa8, 0x78, 0xf2, 0x5b, 0x00, 0x03, 0x74, 0x31, 0x2e, 0x66, 0x61, 0x00,
+        0x6d, 0x4b, 0x39, 0x0a, 0x80, 0x30, 0x10, 0xec, 0xf3, 0x8a, 0x7c, 0x41, 0xed, 0x85, 0x65, 0x8b,
+        0x2d, 0x6c, 0xf7, 0x03, 0x21, 0x44, 0x0d, 0x88, 0x68, 0x3c, 0x2a, 0x1f, 0xef, 0x44, 0x11, 0x2c,
+        0x02, 0x33, 0x30, 0x67, 0xeb, 0xc7, 0x54, 0x19, 0xe7, 0x87, 0x7d, 0x70, 0xa0, 0x37, 0x2d, 0x82,
+        0xfa, 0x17, 0x94, 0x54, 0xb1, 0x7e, 0x9e, 0x8d, 0x1d, 0xd3, 0x76, 0xda, 0xcb, 0x76, 0x71, 0x8a,
+        0x7d, 0x84, 0x58, 0x5c, 0xda, 0xa3, 0x9b, 0xec, 0x16, 0xd6, 0x23, 0xcc, 0x3e, 0x18, 0x26, 0x15,
+        0x55, 0x02, 0x98, 0x48, 0x14, 0x10, 0xca, 0x56, 0x85, 0x99, 0x95, 0xe8, 0x03, 0x82, 0xb7, 0x84,
+        0x61, 0x4c, 0xb1, 0xe0, 0x7c, 0xc9, 0x52, 0xd8, 0xdc, 0x43, 0x6d, 0x5b, 0xc0, 0xb9, 0x00, 0x00,
+        0x00
+    ];
+    auto testFaGz = tempDir.buildPath("test.fa.gz");
+    scope(exit) testFaGz.remove;
+    File(testFaGz, "w").rawWrite(gzip);
+    assert ( FastaIterator(testFaGz).gzipped == true );
+    records = FastaIterator(testFaGz).array;
+}
+
 auto fastaRecords(string filename) {
 
     File f = File(filename);
     FastaRecord[] records; 
     string lineTerm = f.byLine(KeepTerminator.yes).take(1).front.endsWith("\r\n") ? "\r\n" : "\n";
     f.seek(0);
-    ulong offset;
+    // ulong offset;
     foreach(line; f.byLine(KeepTerminator.no, lineTerm)) {
-        offset+= line.length + lineTerm.length;
+        // offset+= line.length + lineTerm.length;
         if ( line.startsWith(">") ) {
             records~=FastaRecord();
             records[$-1].lineTerm = lineTerm;
